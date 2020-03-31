9947ef3c84d95ec258bba35dc42b4868bd327696
Pumpking <gn131131@sina.com>
2020-03-31 13:41:22 +0800

    add mock_modules in production enviroment

--- src/app/delon.module.ts
+++ src/app/delon.module.ts
@@ -1,3 +1,11 @@
+/*
+ * @Description:
+ * @Version: 1.0
+ * @Autor: Pumpking
+ * @Date: 2020-03-27 17:32:57
+ * @LastEditors: Pumpking
+ * @LastEditTime: 2020-03-31 13:39:56
+ */
 /**
  * 进一步对基础模块的导入提炼
  * 有关模块注册指导原则请参考：https://ng-alain.com/docs/module
@@ -12,7 +20,7 @@ import { DelonACLModule } from '@delon/acl';
 import { DelonMockModule } from '@delon/mock';
 import * as MOCKDATA from '../../_mock';
 import { environment } from '@env/environment';
-const MOCK_MODULES = !environment.production ? [DelonMockModule.forRoot({ data: MOCKDATA })] : [];
+const MOCK_MODULES = [DelonMockModule.forRoot({ data: MOCKDATA })];
 // #endregion
 
 // #region reuse-tab
